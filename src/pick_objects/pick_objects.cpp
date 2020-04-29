0b2c704a696d5f2baba4c5e0517b14d5009fad70
Unknown <unknown@example.com>
2020-04-29 08:46:20 +0000

    added pickup node

---
+++ src/pick_objects/pick_objects.cpp
@@ -0,0 +1,89 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  // ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "pick_objects");
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+  bool pickedUp = false;
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = -5.0;
+    goal.target_pose.pose.position.y = 0;
+  goal.target_pose.pose.orientation.w = 1.5707;
+
+  // Define a position and orientation for the robot to reach
+  // goal.target_pose.pose.position.x = 3.7;
+  // goal.target_pose.pose.orientation.w = -0.5;
+
+  // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal - pickup");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+  ROS_INFO("Hooray, the base picked up the object");
+  pickedUp = true;
+}
+  else
+    ROS_INFO("The base failed to pick up");
+
+
+  if(pickedUp)  {
+    ros::Duration(5.0).sleep();
+
+
+
+    // duplicate the original code to add second goal, which will be the drop off
+
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    // Define a position and orientation for the robot to reach
+    goal.target_pose.pose.position.x = 3.7;
+    goal.target_pose.pose.position.y = -0.5;
+    goal.target_pose.pose.orientation.w = 1.5707;
+
+
+    // Send the goal position and orientation for the robot to reach
+    ROS_INFO("Sending goal - dropoff");
+    ac.sendGoal(goal);
+
+    // Wait an infinite time for the results
+    ac.waitForResult();
+
+    // Check if the robot reached its goal
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Hooray, the base dropped off the object");
+      ros::Duration(5.0).sleep();
+    }
+    else {
+      ROS_INFO("The base failed to drop off");
+    }
+
+  }
+  else {
+    ROS_INFO("Drop off not attempted without pickup");
+
+  }
+
+  return 0;
+}
